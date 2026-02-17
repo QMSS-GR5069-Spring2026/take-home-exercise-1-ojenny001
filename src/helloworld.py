a92d9b815f49517847c371be3184972858f2d619
GorataB <gbm2118@columbia.edu>
2026-02-17 11:12:30 -0500

    Turn code into user defined function

--- src/helloworld.py
+++ src/helloworld.py
@@ -1,4 +1,5 @@
 def hello_world(string):
     if string == "Hello, world":
         print("Hello, world!")
-    else print("This is not Hello World")
+    else:
+        print("This is not Hello World")
